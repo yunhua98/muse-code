2da6c8a9b356507adeb93cf8798bcda9f5c70bc1
zappyfish <liamkelly2015@gmail.com>
2016-11-12 13:37:17 -0500

    almost got dem blinks

--- app/src/main/java/com/yzhao/musecode/MainActivity.java
+++ app/src/main/java/com/yzhao/musecode/MainActivity.java
@@ -1,7 +1,11 @@
 package com.yzhao.musecode;
 
 
+import android.Manifest;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v4.app.ActivityCompat;
@@ -11,6 +15,7 @@ import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -42,7 +47,7 @@ import java.util.List;
 
 import static com.choosemuse.libmuse.MuseDataPacketType.ACCELEROMETER;
 
-public class MainActivity extends Activity implements View.OnClickListener{
+public class MainActivity extends Activity implements View.OnClickListener {
 
     // used for logging
     private final String TAG = "MuseCode";
@@ -58,6 +63,7 @@ public class MainActivity extends Activity implements View.OnClickListener{
 
     // This is how we get our data
     private MuseDataListener dataListener;
+
 
     private final double[] accelBuffer = new double[3];
     private boolean accelStale;
@@ -93,16 +99,25 @@ public class MainActivity extends Activity implements View.OnClickListener{
         manager.setContext(this);
 
         // listener for connection state changes
-        WeakReference<MainActivity> weakActivity = new WeakReference<->(this);
+        WeakReference<MainActivity> weakActivity = new WeakReference<>(this);
         connectionListener = new ConnectionListener(weakActivity);
 
 
-        setContentView(R.layout.activity_main);
+        dataListener = new DataListener(weakActivity);
+        // Register a listener to receive notifications of what Muse headbands
+        // we can connect to.
+        manager.setMuseListener(new MuseL(weakActivity));
+
+        ensurePermissions();
+
+        initUI();
+
+        handler.post(tickUi);
     }
 
     @Override
     public void onClick(View v) {
-        if(v.getId() == R.id.connect) {
+        if (v.getId() == R.id.connect) {
             manager.stopListening();
 
             List<Muse> availableMuses = manager.getMuses();
@@ -120,27 +135,58 @@ public class MainActivity extends Activity implements View.OnClickListener{
                 // receive data packets of that type.
                 muse.unregisterAllListeners();
                 muse.registerConnectionListener(connectionListener);
-                muse.registerDataListener(dataListener, MuseDataPacketType.EEG);
-                muse.registerDataListener(dataListener, MuseDataPacketType.ALPHA_RELATIVE);
+                muse.registerDataListener(dataListener, MuseDataPacketType.ARTIFACTS);
                 muse.registerDataListener(dataListener, MuseDataPacketType.ACCELEROMETER);
-                muse.registerDataListener(dataListener, MuseDataPacketType.BATTERY);
-                muse.registerDataListener(dataListener, MuseDataPacketType.DRL_REF);
-                muse.registerDataListener(dataListener, MuseDataPacketType.QUANTIZATION);
+
 
                 // Initiate a connection to the headband and stream the data asynchronously.
                 muse.runAsynchronously();
             }
         }
+        else if(v.getId() == R.id.refresh) {
+            manager.stopListening();
+            manager.startListening();
+        }
+        else if (v.getId() == R.id.disconnect) {
+
+            // The user has pressed the "Disconnect" button.
+            // Disconnect from the selected Muse.
+            if (muse != null) {
+                muse.disconnect(false);
+            }
+
+        } else if (v.getId() == R.id.pause) {
+
+            // The user has pressed the "Pause/Resume" button to either pause or
+            // resume data transmission.  Toggle the state and pause or resume the
+            // transmission on the headband.
+            if (muse != null) {
+                dataTransmission = !dataTransmission;
+                muse.enableDataTransmission(dataTransmission);
+            }
         }
     }
+
+    /**
+     * You will receive a callback to this method each time a headband is discovered.
+     * In this example, we update the spinner with the MAC address of the headband.
+     */
+    public void museListChanged() {
+        final List<Muse> list = manager.getMuses();
+        spinnerAdapter.clear();
+        for (Muse m : list) {
+            spinnerAdapter.add(m.getName() + " - " + m.getMacAddress());
+        }
+    }
+
     public void receiveMuseDataPacket(final MuseDataPacket p, final Muse muse) {
         // valuesSize returns the number of data values contained in the packet.
         final long n = p.valuesSize();
         // check if it's accelerometer data
-        if(p.packetType() == ACCELEROMETER) {
-                assert(accelBuffer.length >= n);
-                getAccelValues(p);
-                accelStale = true;
+        if (p.packetType() == ACCELEROMETER) {
+            assert (accelBuffer.length >= n);
+            getAccelValues(p);
+            accelStale = true;
         }
     }
 
@@ -152,7 +198,7 @@ public class MainActivity extends Activity implements View.OnClickListener{
 
     // update the displayed EKG values
     public void updateEKG() {
-        TextView blink = (TextView)findViewById(R.id.blink);
+        TextView blink = (TextView) findViewById(R.id.blink);
         blink.setText(String.format("blink:%d\n", blink));
     }
 
@@ -161,6 +207,69 @@ public class MainActivity extends Activity implements View.OnClickListener{
         accelBuffer[1] = p.getAccelerometerValue(Accelerometer.Y);
         accelBuffer[2] = p.getAccelerometerValue(Accelerometer.Z);
     }
+
+    /**
+     * You will receive a callback to this method each time there is a change to the
+     * connection state of one of the headbands.
+     *
+     * @param p    A packet containing the current and prior connection states
+     * @param muse The headband whose state changed.
+     */
+    public void receiveMuseConnectionPacket(final MuseConnectionPacket p, final Muse muse) {
+
+        final ConnectionState current = p.getCurrentConnectionState();
+
+        final String status = p.getPreviousConnectionState() + " -> " + current;
+        // Update the UI with the change in connection state.
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+
+                final TextView statusText = (TextView) findViewById(R.id.con_status);
+                statusText.setText(status);
+
+                final MuseVersion museVersion = muse.getMuseVersion();
+                final TextView museVersionText = (TextView) findViewById(R.id.version);
+                // If we haven't yet connected to the headband, the version information
+                // will be null.  You have to connect to the headband before either the
+                // MuseVersion or MuseConfiguration information is known.
+                if (museVersion != null) {
+                    final String version = museVersion.getFirmwareType() + " - "
+                            + museVersion.getFirmwareVersion() + " - "
+                            + museVersion.getProtocolVersion();
+                    museVersionText.setText(version);
+                } /*else {
+                    museVersionText.setText(R.string.undefined);
+                }
+                */
+            }
+        });
+
+        if (current == ConnectionState.DISCONNECTED) {
+            // We have disconnected from the headband, so set our cached copy to null.
+            this.muse = null;
+        }
+    }
+
+
+
+    /**
+     * Classes go here;
+     */
+
+    class MuseL extends MuseListener {
+        final WeakReference<MainActivity> activityRef;
+
+        MuseL(final WeakReference<MainActivity> activityRef) {
+            this.activityRef = activityRef;
+        }
+
+        @Override
+        public void museListChanged() {
+            activityRef.get().museListChanged();
+        }
+    }
+
     class ConnectionListener extends MuseConnectionListener {
         final WeakReference<MainActivity> activityRef;
 
@@ -173,4 +282,85 @@ public class MainActivity extends Activity implements View.OnClickListener{
             activityRef.get().receiveMuseConnectionPacket(p, muse);
         }
     }
+
+    // datalistener class goes here:
+
+    class DataListener extends MuseDataListener {
+        final WeakReference<MainActivity> activityRef;
+
+
+        DataListener(final WeakReference<MainActivity> activityRef) {
+            this.activityRef = activityRef;
+        }
+
+        @Override
+        public void receiveMuseDataPacket(final MuseDataPacket p, final Muse muse) {
+            activityRef.get().receiveMuseDataPacket(p, muse);
+        }
+
+        @Override
+        public void receiveMuseArtifactPacket(final MuseArtifactPacket p, final Muse muse) {
+            activityRef.get().receiveMuseArtifactPacket(p, muse);
+        }
+    }
+
+    // we initialize the UI here
+    private void initUI() {
+        setContentView(R.layout.activity_main);
+        Button refreshButton = (Button) findViewById(R.id.refresh);
+        refreshButton.setOnClickListener(this);
+        Button connectButton = (Button) findViewById(R.id.connect);
+        connectButton.setOnClickListener(this);
+        Button disconnectButton = (Button) findViewById(R.id.disconnect);
+        disconnectButton.setOnClickListener(this);
+        Button pauseButton = (Button) findViewById(R.id.pause);
+        pauseButton.setOnClickListener(this);
+
+        spinnerAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item);
+        Spinner musesSpinner = (Spinner) findViewById(R.id.muses_spinner);
+        musesSpinner.setAdapter(spinnerAdapter);
+    }
+
+    /**
+     * The ACCESS_COARSE_LOCATION permission is required to use the
+     * Bluetooth Low Energy library and must be requested at runtime for Android 6.0+
+     * On an Android 6.0 device, the following code will display 2 dialogs,
+     * one to provide context and the second to request the permission.
+     * On an Android device running an earlier version, nothing is displayed
+     * as the permission is granted from the manifest.
+     *
+     * If the permission is not granted, then Muse 2016 (MU-02) headbands will
+     * not be discovered and a SecurityException will be thrown.
+     */
+    private void ensurePermissions() {
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+        {
+            // We don't have the ACCESS_COARSE_LOCATION permission so create the dialogs asking
+            // the user to grant us the permission.
+
+            DialogInterface.OnClickListener buttonListener =
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which){
+                            dialog.dismiss();
+                            ActivityCompat.requestPermissions(MainActivity.this,
+                                    new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                                    0);
+                        }
+                    };
+
+            // This is the context dialog which explains to the user the reason we are requesting
+            // this permission.  When the user presses the positive (I Understand) button, the
+            // standard Android permission dialog will be displayed (as defined in the button
+            // listener above).
+            AlertDialog introDialog = new AlertDialog.Builder(this)
+                    .setTitle(R.string.permission_dialog_title)
+                    .setMessage(R.string.permission_dialog_description)
+                    .setPositiveButton(R.string.permission_dialog_understand, buttonListener)
+                    .create();
+            introDialog.show();
+        }
+    }
 }
+
